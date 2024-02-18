0d7198fe762827bafe0861a887627b94d3c92668
ericstengard <eric199610@gmail.com>
2024-02-18 23:30:09 +0100

    Add nvim hardtime plugin

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -75,6 +75,13 @@ return packer.startup(function(use)
     "nvim-treesitter/nvim-treesitter",
     run = ":TSUpdate",
   }
+
+  -- Hardtime
+  use {
+    "m4xshen/hardtime.nvim",
+    dependencies = { "MunifTanjim/nui.nvim"},
+  }
+
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
   if PACKER_BOOTSTRAP then
--- .config/nvim/lua/treesitter.lua
+++ .config/nvim/lua/treesitter.lua
@@ -17,4 +17,3 @@ configs.setup {
     additional_vim_regex_highlighting = true,
   },
 }
-
