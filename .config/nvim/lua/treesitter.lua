822d74179ae2561496ab8bd3e5f6e3c1d2947314
ericstengard <eric199610@gmail.com>
2024-02-18 22:05:24 +0100

    Remove debug comments

--- .config/nvim/lua/treesitter.lua
+++ .config/nvim/lua/treesitter.lua
@@ -1,17 +1,3 @@
--- local configs = require("nvim-treesitter.configs")
--- configs.setup {
---   ensure_installed = "all",
---   sync_install = false,
---   ignore_install = { "" }, -- List of parsers to ignore installing
---   highlight = {
---     enable = true, -- false will disable the whole extension
---     disable = { "" }, -- list of language that will be disabled
---     additional_vim_regex_highlighting = true,
-
---   },
---   indent = { enable = true, disable = { "yaml" } },
--- }
-
 local configs = require("nvim-treesitter.configs")
 configs.setup {
   -- A list of parser names, or "all" (the five listed parsers should always be installed)
@@ -31,3 +17,4 @@ configs.setup {
     additional_vim_regex_highlighting = true,
   },
 }
+
